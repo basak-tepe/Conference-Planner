8ba25785f9f10ffda38e0cbb8f2a7f9be4263e5e
Başak Tepe <basak.tepe@cloudflight.io>
2023-07-14 12:22:06 +0300

    Implemented the event service

---
+++ src/main/java/com/conferences/schedule/service/EventServiceImpl.java
@@ -0,0 +1,37 @@
+package com.conferences.schedule.service;
+
+import com.conferences.schedule.model.Event;
+import com.conferences.schedule.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventServiceImpl implements EventService {
+    //repository
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Override
+    public Event addEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public Event getEventById(long id) {
+        return eventRepository.findById(id).get();
+    }
+
+    @Override
+    public List<Event> getEvents() {
+        return (List<Event>) eventRepository.findAll();
+    }
+
+    @Override
+    public void deleteEvent(long id) {
+        eventRepository.deleteById(id);
+
+    }
+}
