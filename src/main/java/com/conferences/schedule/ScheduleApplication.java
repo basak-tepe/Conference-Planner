a047ae76091a1f110fbebaf39f9307f56cf0a7d5
Goksel Ali Pehlivan <goeksel.pehlivan@cloudflight.io>
2023-07-14 14:18:05 +0300

    Updated database configuration

--- src/main/java/com/conferences/schedule/ScheduleApplication.java
+++ src/main/java/com/conferences/schedule/ScheduleApplication.java
@@ -1,13 +1,19 @@
 package com.conferences.schedule;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication (scanBasePackages = "com.conferences.schedule")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableMongoRepositories
 public class ScheduleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScheduleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class, args);
+    }
 
 }
--- src/main/java/com/conferences/schedule/repository/EventRepository.java
+++ src/main/java/com/conferences/schedule/repository/EventRepository.java
@@ -1,11 +1,9 @@
 package com.conferences.schedule.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import com.conferences.schedule.model.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends CrudRepository<Event,Long> {
-
-
+public interface EventRepository extends MongoRepository<Event, Long> {
 }
