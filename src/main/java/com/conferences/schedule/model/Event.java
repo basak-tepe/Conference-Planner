af94f48d5d87ee910cbac329ae228409d993782e
Başak Tepe <basak.tepe@cloudflight.io>
2023-07-19 11:20:58 +0300

    Initialized frontend

--- src/main/java/com/conferences/schedule/model/Event.java
+++ src/main/java/com/conferences/schedule/model/Event.java
@@ -2,9 +2,7 @@ package com.conferences.schedule.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
