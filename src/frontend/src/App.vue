3ce3d8cb77ad6453358c1eab2a3ab4ba8fb8cf29
Başak Tepe <basak.tepe@cloudflight.io>
2023-07-25 10:46:17 +0300

    Added a timeline component from primevue

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,21 +1,40 @@
 <script>
 import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
 import Calendar from 'primevue/calendar';
 import InputText from 'primevue/inputtext';
 import 'primevue/resources/themes/lara-light-purple/theme.css' // import the dark-blue theme
 import Button from 'primevue/button';
 import 'primeicons/primeicons.css';
+import Timeline from 'primevue/timeline';
+import  Card  from 'primevue/card';
+import 'primeicons/primeicons.css';
 
+/**
+ * next week:
+ * fetch all event data as text and view it in front-end
+ * front-to-back is done (post)
+ * now try back-to-front (get)
+ * after that use primevue/timeline
+ */
 export default {
   name:"App",
   components:{
     TheWelcome,
     Calendar,
     InputText,
-    Button
+    Button,
+    Card,
+    Timeline
   },
   data() {
     return {
+      events: [
+        { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
+        { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+        { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
+        { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
+      ],
       msg: '',
       date: null,       // To store the selected date from the Calendar
       title: '',        // To store the title from the InputText
@@ -75,7 +94,7 @@ export default {
 
 <template>
   <!--h1 class="green">{{ msg }}</h1-->
-  <header>
+      <header>
     <div class="wrapper">
       <form @submit.prevent="handleSubmit">
       <div class="inputs">
@@ -90,9 +109,34 @@ export default {
     </div>
 
   </header>
-
   <main>
-    <TheWelcome/>
+    <div class="card">
+      <Timeline :value="events" align="alternate" class="customized-timeline">
+        <template #marker="slotProps">
+              <span class="flex w-2rem h-2rem align-items-center justify-content-center text-white border-circle z-1 shadow-1" :style="{ backgroundColor: slotProps.item.color }">
+                  <i :class="slotProps.item.icon"></i>
+              </span>
+        </template>
+        <template #content="slotProps">
+          <Card class="custom-card-width">
+            <template #title>
+              {{ slotProps.item.status }}
+            </template>
+            <template #subtitle>
+              {{ slotProps.item.date }}
+            </template>
+            <template #content>
+              <!--img v-if="slotProps.item.image" :src="`https://primefaces.org/cdn/primevue/images/product/${slotProps.item.image}`" :alt="slotProps.item.name" width="200" class="shadow-1" /-->
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate
+                neque quas!
+              </p>
+              <Button label="Read more" text></Button>
+            </template>
+          </Card>
+        </template>
+      </Timeline>
+    </div>
   </main>
 </template>
 
@@ -124,7 +168,7 @@ header {
   }
 
   p {
-    font-size: 200%;
+    //font-size: 200%;
   }
 
   .inputs{
@@ -133,5 +177,11 @@ header {
     padding-bottom: 30px;
     gap: 25px;
   }
+
+  .customized-timeline{
+    margin: 60px;
+  }
+
+
 }
 </style>
