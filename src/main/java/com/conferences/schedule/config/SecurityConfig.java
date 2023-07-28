d0c375f0f3e85a559e47de6bdfb993d265ddf694
Başak Tepe <basak.tepe@cloudflight.io>
2023-07-28 13:23:14 +0300

    Added SecurityConfig file and user details service

---
+++ src/main/java/com/conferences/schedule/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.conferences.schedule.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+    //AUTHENTICATION
+    @Bean
+    //users we hardcoded
+    //equivalent of the configure method in spring boot 2
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("basak")
+                .password(encoder.encode("basak"))
+                .roles("ADMIN")
+                .build();
+
+
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+
+//    //AUTHORIZATION
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        //everyone can access the event list
+//        http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/api/events/events").permitAll()
+//                        .requestMatchers("/api/events/add").authenticated()
+//                );
+//
+//        return http.build();
+//
+//
+//    }
+
+    //password encoding
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
