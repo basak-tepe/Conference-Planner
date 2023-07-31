6658b3797cf12158fd67d902e3d7308698bfee41
Goksel Ali Pehlivan <goeksel.pehlivan@cloudflight.io>
2023-07-31 14:42:11 +0300

    Upadeted event rest controller

--- src/main/java/com/conferences/schedule/controller/EventController.java
+++ src/main/java/com/conferences/schedule/controller/EventController.java
@@ -3,24 +3,18 @@ package com.conferences.schedule.controller;
 import com.conferences.schedule.model.Event;
 import com.conferences.schedule.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-//import com.conferences.schedule.basicauth.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping ("/api/events")
+@RequestMapping("/api/events")
 @CrossOrigin(origins = "http://localhost:5173") // frontend URL
-
-
 public class EventController {
     //service
-    private EventService eventService;
+    private final EventService eventService;
 
     @Autowired
     public EventController(EventService eventService) {
@@ -38,42 +32,41 @@ public class EventController {
         // return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed!");
     }
 
-
-    @RequestMapping("/hello")
-    public String hello_world(){
+    @GetMapping("/hello")
+    public String hello_world() {
         return "Hello World";
     }
 
     //add event
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.CREATED)
-    public String addEvent(@RequestBody Event event){
-    eventService.addEvent(event);
-    return "Event added successfully!";
+    public String addEvent(@RequestBody Event event) {
+        eventService.addEvent(event);
+        return "Event added successfully!";
     }
 
-    @RequestMapping("/events")
-    public List<Event> getEvents(){
+    @GetMapping("/events")
+    public List<Event> getEvents() {
         return eventService.getEvents();
     }
 
 
     //getEventById
-    @RequestMapping("/event/{id}")
-    public Event getEventById(@PathVariable int id){
+    @GetMapping("/event/{id}")
+    public Event getEventById(@PathVariable int id) {
         return eventService.getEventById(id);
     }
     //getEvents
 
     //update event
     @PutMapping("/event")
-    public Event updateEvent(@RequestBody Event event){
+    public Event updateEvent(@RequestBody Event event) {
         return eventService.updateEvent(event);
     }
 
     //delete an event
     @DeleteMapping("/event/{id}")
-    public String deleteEvent(@PathVariable long id){
+    public String deleteEvent(@PathVariable long id) {
         eventService.deleteEvent(id);
         return "Event deleted successfully.";
     }
